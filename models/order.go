35b94e4455a38588b282de48f1aeebe93d6449ff
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-23 18:21:31 +0700

    refactor: add pointer to courer id and admin id, add attributes quantity

--- models/order.go
+++ models/order.go
@@ -7,13 +7,14 @@ import (
 type Order struct {
 	gorm.Model
 	CustomerID uint    `json:"customer_id"`
-	CourierID  uint    `json:"courier_id"`
-	AdminID    uint    `json:"admin_id"`
+	CourierID  *uint   `json:"courier_id"`
+	AdminID    *uint   `json:"admin_id"`
 	ServiceID  uint    `json:"service_id"`
 	AddressID  uint    `json:"address_id"`
-	Weight     float64 `json:"weight"`
+	Weight     float64 `json:"weight,omitempty"`
+	Quantity   int     `json:"quantity,omitempty"` 
 	TotalPrice float64 `json:"total_price"`
-	Status     string  `json:"status"` // "waiting for pickup", "in process", "out for delivery", "completed"
+	Status     string  `json:"status"` 
 	Address    Address `json:"address" gorm:"foreignKey:AddressID"`
 	Customer   User    `json:"customer" gorm:"foreignKey:CustomerID"`
 	Courier    User    `json:"courier" gorm:"foreignKey:CourierID"`
