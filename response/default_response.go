2096783c016b036e0a069a30a256d921b1b7a192
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-23 18:21:56 +0700

    refactor: change order of response

--- response/default_response.go
+++ response/default_response.go
@@ -2,8 +2,8 @@ package response
 
 // AddressResponse represents address data without timestamps
 type DefaultResponse struct {
+	Code    int         `json:"code"`
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
-	Code    int         `json:"code"`
 	Data    interface{} `json:"data"`
 }
