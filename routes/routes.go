d58639457eb321170b902009bbdf0d57eb97e608
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-23 18:22:50 +0700

    refactor: delete unused comment, add grup for customer

--- routes/routes.go
+++ routes/routes.go
@@ -1,61 +1,11 @@
-// package routes
-
-// import (
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/raihansyahrin/backend_laundry_app.git/controllers"
-// 	"github.com/raihansyahrin/backend_laundry_app.git/middlewares"
-// )
-
-// func SetupRoutes(router *gin.Engine) {
-// 	authRoutes := router.Group("api/auth")
-// 	{
-// 		authRoutes.POST("/register", controllers.Register)
-// 		authRoutes.POST("/login", controllers.Login)
-// 	}
-
-// 	userRoutes := router.Group("api/users")
-// 	{
-// 		userRoutes.GET("/", middlewares.AuthMiddleware(), controllers.GetUsers)
-// 		userRoutes.GET("/:id", middlewares.AuthMiddleware(), controllers.GetUser)
-// 		userRoutes.PUT("/:id", middlewares.AuthMiddleware(), controllers.UpdateUser)
-// 		userRoutes.DELETE("/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
-// 	}
-
-// 	orderRoutes := router.Group("api/orders")
-// 	{
-// 		orderRoutes.POST("/", middlewares.AuthMiddleware(), controllers.CreateOrder)
-// 		orderRoutes.GET("/", middlewares.AuthMiddleware(), controllers.GetOrders)
-// 		orderRoutes.PUT("/status", middlewares.AuthMiddleware(), controllers.UpdateOrderStatus)
-// 		orderRoutes.DELETE("/", middlewares.AuthMiddleware(), controllers.DeleteOrder)
-// 		orderRoutes.POST("/accept/:id", middlewares.AuthMiddleware(), controllers.AcceptOrder) // New route for accepting orders
-
-// 	}
-
-// 	serviceRoutes := router.Group("api/services")
-// 	{
-// 		serviceController := &controllers.ServiceController{}
-// 		serviceRoutes.GET("/", serviceController.GetServices)
-// 		serviceRoutes.GET("/:id", serviceController.GetServiceByID)
-// 		serviceRoutes.POST("/", serviceController.CreateService)
-// 		serviceRoutes.PUT("/:id", serviceController.UpdateService)
-// 		serviceRoutes.DELETE("/:id", serviceController.DeleteService)
-// 		serviceRoutes.GET("/category/:category", serviceController.GetServiceByCategory)
-// 	}
-
-//		addressRoutes := router.Group("api/addresses")
-//		{
-//			addressController := &controllers.AddressController{}
-//			addressRoutes.POST("/", middlewares.AuthMiddleware(), addressController.CreateAddress)
-//			addressRoutes.GET("/user/:user_id", middlewares.AuthMiddleware(), addressController.GetAddressesByUserID)
-//			addressRoutes.PUT("/:id", middlewares.AuthMiddleware(), addressController.UpdateAddress)
-//			addressRoutes.DELETE("/:id", middlewares.AuthMiddleware(), addressController.DeleteAddress)
-//		}
-//	}
 package routes
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/raihansyahrin/backend_laundry_app.git/controllers"
+	admin_controllers "github.com/raihansyahrin/backend_laundry_app.git/controllers/admin"
+	courier_controllers "github.com/raihansyahrin/backend_laundry_app.git/controllers/courier"
+	customer_controller "github.com/raihansyahrin/backend_laundry_app.git/controllers/customer"
 	"github.com/raihansyahrin/backend_laundry_app.git/middlewares"
 )
 
@@ -74,23 +24,54 @@ func SetupRoutes(router *gin.Engine) {
 		userRoutes.DELETE("/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
 	}
 
+	customerGroup := router.Group("api/customers")
+	{
+		customerGroup.GET("/", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer"), customer_controller.GetCustomers)
+		customerGroup.GET("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer"), customer_controller.GetCustomer)
+		customerGroup.PUT("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer"), customer_controller.UpdateCustomer)
+		customerGroup.DELETE("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer"), customer_controller.DeleteCustomer)
+	}
+
+	courierGroup := router.Group("api/couriers")
+	{
+		courierGroup.GET("/", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("couriers"), courier_controllers.GetCouriers)
+		courierGroup.GET("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("couriers"), courier_controllers.GetCourier)
+		courierGroup.PUT("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("couriers"), courier_controllers.UpdateCourier)
+		courierGroup.DELETE("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("couriers"), courier_controllers.DeleteCourier)
+	}
+
+	adminGroup := router.Group("api/admins")
+	{
+		adminGroup.GET("/", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), admin_controllers.GetAdmins)
+		adminGroup.GET("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), admin_controllers.GetAdmin)
+		adminGroup.PUT("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), admin_controllers.UpdateAdmin)
+		adminGroup.DELETE("/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), admin_controllers.DeleteAdmin)
+	}
+
 	orderRoutes := router.Group("api/orders")
 	{
-		orderRoutes.POST("/", middlewares.AuthMiddleware(), controllers.CreateOrder)
 		orderRoutes.GET("/", middlewares.AuthMiddleware(), controllers.GetOrders)
-		orderRoutes.GET("/:customer_id", middlewares.AuthMiddleware(), controllers.GetOrderDetailForCustomer) // Updated route for getting order details for customer
-
 		orderRoutes.PUT("/status", middlewares.AuthMiddleware(), controllers.UpdateOrderStatus)
 		orderRoutes.DELETE("/", middlewares.AuthMiddleware(), controllers.DeleteOrder)
-		// orderRoutes.POST("/accept/:id", middlewares.AuthMiddleware(), controllers.AcceptOrder)         // Route for accepting orders
-		orderRoutes.POST("/courier-arrived", middlewares.AuthMiddleware(), controllers.CourierArrived) // Route for courier arrived and updating weight
-		orderRoutes.POST("/accept/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), controllers.AcceptOrder)
-		orderRoutes.POST("/payment", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer", "courier"), controllers.ProcessPayment)
+		orderRoutes.POST("/payment", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer", "courier"), customer_controller.ProcessPayment)
+
+		//Customer
+		orderRoutes.POST("/", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("customer"), customer_controller.CreateOrder)
+		orderRoutes.GET("/:customer_id", middlewares.AuthMiddleware(), customer_controller.GetOrderDetailForCustomer)
+
+		//Courier
+		orderRoutes.POST("/accept/:id", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("courier"), courier_controllers.AcceptOrder)
+		orderRoutes.POST("/courier-arrived", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("courier"), courier_controllers.CourierArrived)
+		orderRoutes.POST("/accept-cash-payment", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("courier"), courier_controllers.AcceptCashPayment)
+		orderRoutes.POST("/order-delivery", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("courier"), courier_controllers.OrderDelivery)
+
+		//Admin
+		orderRoutes.POST("/order-complete", middlewares.AuthMiddleware(), middlewares.RoleMiddleware("admin"), admin_controllers.OrderComplete)
 	}
 
 	serviceRoutes := router.Group("api/services")
 	{
-		serviceController := &controllers.ServiceController{}
+		serviceController := &admin_controllers.ServiceController{}
 		serviceRoutes.GET("/", serviceController.GetServices)
 		serviceRoutes.GET("/:id", serviceController.GetServiceByID)
 		serviceRoutes.POST("/", serviceController.CreateService)
@@ -101,7 +82,7 @@ func SetupRoutes(router *gin.Engine) {
 
 	addressRoutes := router.Group("api/addresses")
 	{
-		addressController := &controllers.AddressController{}
+		addressController := &customer_controller.AddressController{}
 		addressRoutes.POST("/", middlewares.AuthMiddleware(), addressController.CreateAddress)
 		addressRoutes.GET("/user/:user_id", middlewares.AuthMiddleware(), addressController.GetAddressesByUserID)
 		addressRoutes.PUT("/:id", middlewares.AuthMiddleware(), addressController.UpdateAddress)
