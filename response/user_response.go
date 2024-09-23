77e00e26eaecfd34a42978ab805d84ed15686237
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-23 18:22:13 +0700

    refactor: delete unused comment

--- response/user_response.go
+++ response/user_response.go
@@ -1,14 +1,5 @@
 package response
 
-// UserResponse represents user data without timestamps
-//
-//	type UserResponse struct {
-//		ID        uint              `json:"id"`
-//		Username  string            `json:"username"`
-//		Email     string            `json:"email"`
-//		Role      string            `json:"role"`
-//		Addresses []AddressResponse `json:"addresses"`
-//	}
 type UserResponse struct {
 	ID        uint               `json:"id"`
 	Username  string             `json:"username"`
