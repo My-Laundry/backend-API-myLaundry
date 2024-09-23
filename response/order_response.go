2c65ebb3acf90ce5be32a89aa73389674b530f0a
Muhamad Raihan Syahrin Sya'bani <raihansyahrin@gmail.com>
2024-09-23 18:22:06 +0700

    refactor: add several attributes

--- response/order_response.go
+++ response/order_response.go
@@ -1,13 +1,16 @@
 package response
 
 type OrderResponse struct {
-	ID        uint            `json:"id"`
-	Status    string          `json:"status"`
-	CreatedAt string          `json:"created_at"`
-	UpdatedAt string          `json:"updated_at"`
-	Customer  UserResponse    `json:"customer"`
-	Courier   UserResponse    `json:"courier"`
-	Admin     UserResponse    `json:"admin"`
-	Service   ServiceResponse `json:"service"`
-	Address   AddressResponse `json:"address"`
+	ID         uint            `json:"id"`
+	Status     string          `json:"status"`
+	CreatedAt  string          `json:"created_at"`
+	UpdatedAt  string          `json:"updated_at"`
+	TotalPrice float64         `json:"total_price,omitempty"`
+	Weight     float64         `json:"weight,omitempty"`
+	Quantity   int             `json:"quantity,omitempty"` // Menambahkan field Quantity
+	Customer   UserResponse    `json:"customer"`
+	Courier    UserResponse    `json:"courier"`
+	Admin      UserResponse    `json:"admin"`
+	Service    ServiceResponse `json:"service"`
+	Address    AddressResponse `json:"address"`
 }
